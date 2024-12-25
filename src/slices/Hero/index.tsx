6749cc1a34c2e504b8ec969295663e09130350bf
kriss <krzysztof.podmokly@gmail.com>
2024-12-25 21:30:40 +0100

    Hero can animation and bubbles

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -10,6 +10,9 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
+import { View } from "@react-three/drei";
+import Scene from "./Scene";
+import { Bubbles } from "./Bubbles";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -58,7 +61,6 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         start: "top top",
         end: "bottom bottom",
         scrub: 1.5,
-        markers: true,
       },
     });
 
@@ -95,6 +97,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="hero opacity-0"
     >
+      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+        <Scene />
+        <Bubbles count={300} speed={2} repeat />
+      </View>
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
